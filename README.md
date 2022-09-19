e287430f1b51093bd2f7745939d8679984a3b91f
hyperschwartz <jschwartz@figure.com>
2022-09-19 16:24:21 -0700

    Add initial concept

---
+++ README.md
@@ -0,0 +1,4 @@
+# Object Store Gateway Contract Attributes
+
+This library includes helper functions for appending event attributes in a [CosmWasm](https://github.com/CosmWasm/cosmwasm)-based
+smart contract.
---
+++ src/attribute_consts.rs
@@ -0,0 +1,7 @@
+pub const EVENT_TYPE_KEY: &str = "object_store_gateway_event_type";
+pub const SCOPE_ADDRESS_KEY: &str = "object_store_gateway_scope_address";
+pub const TARGET_ACCOUNT_KEY: &str = "object_store_gateway_target_account_address";
+pub const ACCESS_GRANT_ID_KEY: &str = "object_store_gateway_access_grant_id";
+
+pub const ACCESS_GRANT_VALUE: &str = "access_grant";
+pub const ACCESS_REVOKE_VALUE: &str = "access_revoke";
